13dd9077a25f8bd717b8553e80ef91d3e68cc714
Jane Venter <janeanneventer@gmail.com>
2020-09-10 18:58:26 +0000

    Automatically backed up by Learn

---
+++ binary_search_tree.rb
@@ -0,0 +1,30 @@
+class BST
+  
+  attr_reader :number, :left, :right
+  
+  def initialize(number)
+    @number = number
+  end
+  
+  def data 
+    @number
+    @left = nil
+    @right = nil
+  end
+  
+  def insert(value)
+    if value <= data && @left == nil
+      @left = BST.new(value)
+    elsif value <= data && @left != nil
+      @left.data.left = BST.new(value)
+    elsif value > data && @right == nil
+      @right = BST.new(value)   
+    end
+  end
+  
+  def each 
+  end
+  
+  def record_all_data 
+  end
+end
